aa1aa66c3bf67e36fa70b2ed7d5f55af8e4bd30f
eugeniusms <eugeniusms@gmail.com>
2022-08-23 12:18:06 +0700

    🔧 Fix Same-Origin Policy

--- src/server.js
+++ src/server.js
@@ -5,6 +5,14 @@ const init = async () => {
   const server = Hapi.server({
     port: 8001,
     host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'], // an array of origins or 'ignore'
+        // Fix issue CORS Policy address chrome :
+        // chrome://flags/#block-insecure-private-network-requests
+        // Disable menu tersebut
+      },
+    },
   });
 
   server.route(routes);
